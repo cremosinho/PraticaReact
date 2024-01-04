ac744da5bda25537e88ed06ac7c931e76752c818
fabricio costa <fabricio-fjc@outlook.com>
2024-01-04 15:12:57 -0300

    refactor no button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,11 @@
 import './styles.css'
 
-export default (props) => (
+export default function Button (props){
+    return(
     
-    <div className={`${props.disabled ? 'ds-button' : 'st-button'}`} onClick={props.disabled ? null : props.clicar}>
-        <h5>{props.text}</h5>
-    </div>
+        <div className={`${props.disabled ? 'ds-button' : 'st-button'}`} onClick={props.disabled ? null : props.clicar}>
+            <h5>{props.text}</h5>
+        </div>
     
-)
+    )
+}
